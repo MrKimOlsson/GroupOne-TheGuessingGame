8cd8377f8c362e36d8227d09cb5e17712454d1a7
Lotta <charlotte.a.nyman@gmail.com>
2022-10-05 11:30:35 +0200

    add a retry button

---
+++ script.js
@@ -0,0 +1,11 @@
+const retry = document.querySelector(".retry");
+const guessedNumbersAre =document.querySelector(".guessedNumbersAre")
+
+retry.addEventListener(click, () => {
+    message.innerHTML = "";
+    
+    // guessedNumbers = []
+     // guessedNumbersAre.innerHTML ="";
+    // showHighScore()
+   
+})
