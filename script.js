b2325a7713e401b72d08e0fe309561abecaf89fb
fogelmark <108455196+fogelmark@users.noreply.github.com>
2022-10-05 11:50:04 +0200

    started on start button

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1
-
 console.log(randomNumber);
+
+const form = document.querySelector('#card')
+const playerGuess = document.querySelector('#playerGuess')
+
+const validateNumber = (input) => {
+        if (input.value == randomNumber) {
+        console.log('rätt');
+    } else if (input.value < randomNumber) {
+        console.log('får lågt');
+    } else if (input.value > randomNumber) {
+        console.log('för högt');
+    }
+
+    input.value = ''
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    validateNumber(playerGuess)
+})
+
