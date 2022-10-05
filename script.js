171845a0a93464b5c236081591e16b6049df090c
Kim Olsson <mr.kim.olsson@gmail.com>
2022-10-06 00:14:13 +0200

    CSS,design,darkmode,output

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-<<<<<<< HEAD
-=======
-// Variables
-const storeGuess = []
-const form = document.querySelector('#card')
+// ------------ DECLAIRATIONS ------------
+// DECLAIR REFERENCES TO HTML ELEMENTS
+const form = document.querySelector('.card');
 const retry = document.querySelector('.btn-retry');
-const btn = document.querySelector('.btn')
-let guessedNumbersAre = document.querySelector(".guessedNumbersAre")
-let playerGuess = document.querySelector('#playerGuess')
-let answer = document.querySelector('.answer')
+const btn = document.querySelector('.btn');
+const darkModeBtn = document.querySelector('.dark-mode-btn');
+let storeGuess = [];
+let guessedNumbersAre = document.querySelector(".guessedNumbersAre");
+let playerGuess = document.querySelector('#playerGuess');
+let answer = document.querySelector('.answer');
 let randomNumber = Math.floor(Math.random() * 100) + 1
 let clicks = null
 console.log(randomNumber, 'random number');
 
 
+// ------------ FUNCTIONS ------------
+
+
+
+
+// HANDLE THE PLAYER GUESS
 const validateNumber = (input) => {
 
+    // COUNTS THE CLICKS
     clicks++
     console.log(clicks, 'number of guesses');
 
+    // IF GUESS IS CORRECT
     if (input.value == randomNumber) {
         changeText(answer, `Correct! It took you: ${clicks} tries.`)
         document.querySelector('.btn').disabled = true;
+
+    // IF GUESS IS TO LOW
     } else if (input.value < randomNumber) {
         changeText(answer, 'Your guess was too low')
+
+    // IF GUESS IS TO HIGH
     } else if (input.value > randomNumber) {
         changeText(answer, 'Your guess was too high')
     }
 
-    if (clicks > 10) {
+    // GAME OVER - IF CLICKS ARE = 10
+    if (clicks == 10) {
         console.error('game over');
         changeText(answer, 'GAME OVER. Restart the game.')
+        btn.disabled = true;
     }
 
-    storeGuess.push(playerGuess.value)
-    guessedNumbersAre.innerText = storeGuess
-    console.log(storeGuess);
+        // STORE THE GUESSES IN AN ARRAY
+        storeGuess.push(playerGuess.value)
+        guessedNumbersAre.innerText = storeGuess;
+        console.log(storeGuess);
     input.value = ''
     
 }
 
+// ------------ TEXT OUTPUT CHANGERS ------------
+
+// CHANGE THE OUTPUT TEXT
 const changeText = (element, text) => {
     element.innerText = text
 }
 changeText(answer, '')
 
-btn.addEventListener('click', (e) => {
+// ------------ LISTENERS ------------
+
+// LISTENING FOR GUESS BUTTON CLICK
+form.addEventListener('submit', (e) => {
     e.preventDefault()
     validateNumber(playerGuess)
 })
 
-retry.addEventListener('click', () => {
+// LISTENING FOR RETRY BUTTON CLICK
+retry.addEventListener('click', (e) => {
+    e.preventDefault()
     console.clear();
     changeText(answer, '')
     changeText(guessedNumbersAre, '')
@@ -57,5 +80,32 @@ retry.addEventListener('click', () => {
     console.log(randomNumber);
     btn.disabled = false;
     clicks = ''
+    storeGuess = []
 })
->>>>>>> f82dd10c5af7ff20552a3c324c304173d12a6769
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DARK MODE
+
+const darkModeFunction = () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+darkModeBtn.addEventListener('click', (e) => {
+    darkModeFunction();
+})
